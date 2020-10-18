d2d07f145a51e5ac2bfd205c22d6a2a8fd050ea4
Igorg599 <igorg599@gmail.com>
2020-10-18 14:12:51 +0500

    commit-2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    
+    //Реализация всплытия отдельных блоков взамен других
+    let more = document.querySelectorAll('.info_item');
+    more.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.querySelector('.info_des').style.display = 'none';
+            item.querySelector('.info_descr').style.display = 'block';
+        });
+        item.addEventListener('mouseout', () => {
+            item.querySelector('.info_descr').style.display = 'none';
+            item.querySelector('.info_des').style.display = 'block';
+        });
+    });
+});
+
 
